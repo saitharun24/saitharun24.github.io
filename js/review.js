9f3e6298f1a5979f25f55233d888a9d4a98af515
A. Sai Tharun <50414959+saitharun24@users.noreply.github.com>
2021-10-22 17:06:01 +0530

    Add files via upload

--- js/review.js
+++ js/review.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', async (event) => {
 	const url = "https://api.telegram.org/bot2059098496:AAFaDwkXt4gtGpbIb-jzsgNhaO3TJzyTb2I/sendMessage?chat_id=785778962&text=";
 	const name = form.elements['name'].value;
 	const msg = form.elements['text'].value;
+	document.getElementById("feedback").reset();
 	const message = encodeURIComponent("<b>Portfolio Site</b>" + "\nName : "+ name +"\nMessage : <i>" + msg +"</i>");
 	const URL = url + message + "&parse_mode=html";
 	var xhttp = new XMLHttpRequest();
